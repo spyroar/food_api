2cbc43d601116e374b56f0ed953ac3cf004a039c
Shivpal <yadavshivpal1999@gmail.com>
2024-12-16 11:46:38 +0530

    project setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
+
+
+app.listen(PORT, () => console.log(`Port listening on ${PORT}!`))
